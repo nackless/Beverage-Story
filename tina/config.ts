d7bbe1a1aed5b23312a9fcc54d719032bd65873a
nackless <108402910+nackless@users.noreply.github.com>
2024-12-16 02:49:38 +0000

    made some more changes to tinacms

--- tina/config.ts
+++ tina/config.ts
@@ -1,30 +1,31 @@
-import { defineConfig } from "tinacms";
+import { title } from "process";
+import { defineConfig, tinaTableTemplate } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+  // process.env.GITHUB_BRANCH ||
+  // process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD || "main";
 
 export default defineConfig({
-  branch: "main",
+  branch,
 
-  // Get this from tina.io
-  clientId: "1efb06e9-53f1-4452-bb54-a224ec8ecc1a",
-  // Get this from tina.io
-  token: "72174db5a58ab883954240f0be621c7f37c23ea7",
+  
+  clientId: "1efb06e9-53f1-4452-bb54-a224ec8ecc1a", // Get this from tina.io
+  token: "72174db5a58ab883954240f0be621c7f37c23ea7", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
     publicFolder: "public",
   },
+
   media: {
     tina: {
       mediaRoot: "",
       publicFolder: "public",
     },
   },
+
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
@@ -32,17 +33,28 @@ export default defineConfig({
         name: "post",
         label: "Posts",
         path: "src/pages/posts",
+        defaultItem: () => ({
+          title: "New Post",
+          layout: "../../layouts/PostLayout.astro",
+        }),
         fields: [
           {
+						name: "layout",
+						label: "Layout",
+						type: "string",
+						required: true,
+						searchable: false,
+					},
+          // {
             
-              label: 'Layout',
-              name: 'layout',
-              type: 'string',
-              options: [
-                { label: 'Post Layout', value: '../../layouts/PostLayout.astro' },
-                // ...other layout options if needed
-              ],
-          },
+          //     label: 'Layout',
+          //     name: 'layout',
+          //     type: 'string',
+          //     options: [
+          //       { label: 'Post Layout', value: '../../layouts/PostLayout.astro' },
+          //       // ...other layout options if needed
+          //     ],
+          // },
           {
             type: "string",
             name: "title",
